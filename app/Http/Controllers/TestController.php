34410bfc8b631a85a9b5e35b94aebae25ec52fbf
xuananfpt <annxph37114@fpt.edu.vn>
2024-07-18 13:49:14 +0700

    Update

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,10 +6,12 @@ use App\Models\Article;
 use App\Models\Comment;
 use App\Models\Video;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
-    function index() {
+    function index()
+    {
         //CÂU 1: Mối quan hệ giữa Article và Comment là 1 - n
         $article = Article::find(4);
         // dd($article->comments);
@@ -24,5 +26,14 @@ class TestController extends Controller
         // dd($averageRating);
         //CÂU 5
         //Câu 6
+        $topRatedArticles = Article::with([
+            'ratings' => function ($query) {
+                $query->select(DB::raw('rateable_id, AVG(rating) as average_rating'))
+                    ->groupBy('rateable_id')
+                    ->orderBy('average_rating', 'desc')
+                    ->take(5);
+            }
+        ])->get();
+        dd($topRatedArticles);
     }
 }
